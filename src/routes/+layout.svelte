e01aaa651be0d006ba779c0f0ebaf7c9b868b865
sothearo-kay <sothearo.kay@techostartup.center>
2025-05-22 16:50:07 +0700

    feat(ui): add view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onNavigate } from '$app/navigation';
+	import { page } from '$app/state';
 	import '../app.css';
 
 	let { children } = $props();
@@ -23,13 +24,31 @@
 </script>
 
 <nav>
-	<ul>
+	<ul class="flex h-10 items-center justify-center">
 		{#each navMenu as navItem (navItem.path)}
-			<li>
-				<a href={navItem.path}>{navItem.label}</a>
+			<li
+				aria-current={page.url.pathname === navItem.path ? 'page' : undefined}
+				class="relative h-full"
+			>
+				<a href={navItem.path} class="flex h-full items-center px-2">{navItem.label}</a>
 			</li>
 		{/each}
 	</ul>
 </nav>
 
-{@render children()}
+<main class="container py-10">{@render children()}</main>
+
+<style>
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid red;
+		view-transition-name: active-page;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <h1>Home page</h1>
-<div>
-	<div>
+
+<div class="flex items-center gap-x-2">
+	<div
+		class="h-16 w-16 overflow-hidden rounded-full bg-neutral-100"
+		style="view-transition-name: avatar;"
+	>
 		<img
-			src="https://api.dicebear.com/9.x/notionists/svg?=seed=sothearo"
+			src="https://api.dicebear.com/9.x/notionists/svg?seed=Aneka"
 			alt="avatar"
 			width="64"
 			height="64"
 		/>
 	</div>
-	<p>Sothearo</p>
+	<h1 class="inline-block" style="view-transition-name: name;">Aneka</h1>
 </div>
