8703933362a128a9c858e5f9003b186c32c2cb91
sothearo-kay <sothearo.kay@techostartup.center>
2025-05-27 08:37:41 +0700

    feat: add footer and constants

---
+++ src/lib/constants/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Sothearo';
+export const description = 'Portfolio and blog';
+export const url = dev ? 'http://localhost:5173/' : 'https://sothearo.github.io/';
