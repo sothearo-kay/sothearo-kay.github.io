bfb785679b3cf1a81a910f21b1e9f18597cd9be0
sothearo-kay <sothearo.kay@techostartup.center>
2025-05-27 10:18:59 +0700

    feat(dev): setup mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md']
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	extensions: ['.svelte', '.md'],
+
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
 		// default options are shown. On some platforms
